96f88a3428b2a21a66cdcfe6fc997d55ef8c3192
TheFlopGuy <theflopguymail@gmail.com>
2020-07-14 02:34:58 +0530

    Wrap Up Day 3
    In Progress - CounterCreate frag setup

---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterData.java
@@ -0,0 +1,31 @@
+package com.theflopguyproductions.ticktrack.ui.counter;
+
+public class CounterData {
+    String lastUpdateTimeStamp;
+    String countValue;
+    String counterLabel;
+
+    public String getLastUpdateTimeStamp() {
+        return lastUpdateTimeStamp;
+    }
+
+    public void setLastUpdateTimeStamp(String lastUpdateTimeStamp) {
+        this.lastUpdateTimeStamp = lastUpdateTimeStamp;
+    }
+
+    public String getCountValue() {
+        return countValue;
+    }
+
+    public void setCountValue(String countValue) {
+        this.countValue = countValue;
+    }
+
+    public String getCounterLabel() {
+        return counterLabel;
+    }
+
+    public void setCounterLabel(String counterLabel) {
+        this.counterLabel = counterLabel;
+    }
+}
--- app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterFragment.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterFragment.java
@@ -9,9 +9,14 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.theflopguyproductions.ticktrack.MainActivity;
@@ -21,16 +26,23 @@ import com.theflopguyproductions.ticktrack.ui.timer.TimerFragment;
 import com.theflopguyproductions.ticktrack.ui.utils.swipebutton.OnActiveListener;
 import com.theflopguyproductions.ticktrack.ui.utils.swipebutton.SwipeButton;
 
+import java.util.ArrayList;
+
 import static android.content.Context.VIBRATOR_SERVICE;
 
 public class CounterFragment extends Fragment {
 
-    private CounterViewModel counterViewModel;
-    private SwipeButton plusButton;
-    private SwipeButton minusButton;
-    private TextView CounterText;
+//    private SwipeButton plusButton;
+//    private SwipeButton minusButton;
+//    private TextView CounterText;
+//    private int newCount=0;
+
     private Vibrator hapticFeed;
-    private int newCount=0;
+
+    private RecyclerView recyclerView;
+    private CounterAdapter counterAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
+    ArrayList<CounterData> counterDataArrayList = new ArrayList<>();
 
     private static final String DISABLED_PARAM = "param2";
 
@@ -55,36 +67,51 @@ public class CounterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        counterViewModel =
-                ViewModelProviders.of(this).get(CounterViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_counter, container, false);
-        CounterText = root.findViewById(R.id.counterText);
-        CounterText.setText(""+newCount);
-        plusButton = root.findViewById(R.id.plusbtn);
-        minusButton = root.findViewById(R.id.minusbtn);
+//        CounterText = root.findViewById(R.id.counterText);
+//        CounterText.setText(""+newCount);
+//        plusButton = root.findViewById(R.id.plusbtn);
+//        minusButton = root.findViewById(R.id.minusbtn);
         hapticFeed = (Vibrator) getActivity().getSystemService(VIBRATOR_SERVICE);
-        plusButton.setOnActiveListener(new OnActiveListener() {
-            @Override
-            public void onActive() {
-                newCount+=1;
-                hapticFeed.vibrate(50);
-                CounterText.setText(""+newCount);
-            }
+//        plusButton.setOnActiveListener(new OnActiveListener() {
+//            @Override
+//            public void onActive() {
+//                newCount+=1;
+//                hapticFeed.vibrate(50);
+//                CounterText.setText(""+newCount);
+//            }
+//
+//        });
+//        minusButton.setOnActiveListener(new OnActiveListener() {
+//            @Override
+//            public void onActive() {
+//                if(newCount>=1){
+//                    hapticFeed.vibrate(50);
+//                    newCount-=1;
+//                    CounterText.setText(""+newCount);
+//                }
+//            }
+//
+//        });
 
-        });
-        minusButton.setOnActiveListener(new OnActiveListener() {
-            @Override
-            public void onActive() {
-                if(newCount>=1){
-                    hapticFeed.vibrate(50);
-                    newCount-=1;
-                    CounterText.setText(""+newCount);
-                }
-            }
-
-        });
+        recyclerView = root.findViewById(R.id.counterRecycleView);
+        counterAdapter = new CounterAdapter(counterDataArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(counterAdapter);
 
 
         return root;
+    }
+
+    public void fabClicked() {
+        CounterData counterData = new CounterData();
+        counterData.setCounterLabel("LABEL");
+        counterData.setCountValue("VALUE");
+        counterData.setLastUpdateTimeStamp("TIMESTAMP");
+        counterDataArrayList.add(counterData);
+        counterAdapter.notifyData(counterDataArrayList);
     }
 }
