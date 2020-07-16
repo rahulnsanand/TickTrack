798fd00ef12287dd6881d6ccf3c5cf2a363b60e9
TheFlopGuy <theflopguymail@gmail.com>
2020-07-16 21:07:53 +0530

    Next Occurence perfected. Toggle Button Perfected. Todo: Fix if date is today, ringtone, design uplift

--- app/src/main/java/com/theflopguyproductions/ticktrack/ui/home/activity/alarm/AlarmCreator.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/home/activity/alarm/AlarmCreator.java
@@ -6,7 +6,6 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,19 +17,20 @@ import com.theflopguyproductions.ticktrack.ui.utils.datepicker.CalendarView;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AlarmCreator extends AppCompatActivity {
 
-    ImageButton alarmBack, calendarDownButton, calendarUpButton;
+    ImageButton alarmBack, calendarDownButton, calendarUpButton, saveButton;
     Button alarmSave;
     TextView tomorrowRingText, nextOccurrence;
     ChipGroup dayChipGroup;
     CalendarView calendarView;
     ArrayList<Date> selectedDates;
-    Map<String, Boolean> repeatDays;
+    Map<Integer, Boolean> repeatDays;
     public static int hour, minute;
     SimpleDateFormat format = new SimpleDateFormat("EE, d MMMM, ''yy");
 
@@ -59,31 +59,32 @@ public class AlarmCreator extends AppCompatActivity {
         thursdayCheck = findViewById(R.id.thursdayToggle);
         fridayCheck = findViewById(R.id.fridayToggle);
         saturdayCheck = findViewById(R.id.saturdayToggle);
+        saveButton = findViewById(R.id.saveButton);
 
         repeatDays = new HashMap<>();
 
         CompoundButton.OnCheckedChangeListener toggleListener = (compoundButton, b) -> {
                     switch(compoundButton.getId()) {
                         case R.id.sundayToggle:
-                            setRepeatDays(compoundButton, "Sunday");
+                            setRepeatDays(compoundButton, Calendar.SUNDAY);
                             break;
                         case R.id.mondayToggle:
-                            setRepeatDays(compoundButton, "Monday");
+                            setRepeatDays(compoundButton, Calendar.MONDAY);
                             break;
                         case R.id.tuesdayToggle:
-                            setRepeatDays(compoundButton, "Tuesday");
+                            setRepeatDays(compoundButton, Calendar.TUESDAY);
                             break;
                         case R.id.wednesdayToggle:
-                            setRepeatDays(compoundButton, "Wednesday");
+                            setRepeatDays(compoundButton, Calendar.WEDNESDAY);
                             break;
                         case R.id.thursdayToggle:
-                            setRepeatDays(compoundButton, "Thursday");
+                            setRepeatDays(compoundButton, Calendar.THURSDAY);
                             break;
                         case R.id.fridayToggle:
-                            setRepeatDays(compoundButton, "Friday");
+                            setRepeatDays(compoundButton, Calendar.FRIDAY);
                             break;
                         case R.id.saturdayToggle:
-                            setRepeatDays(compoundButton, "Saturday");
+                            setRepeatDays(compoundButton, Calendar.SATURDAY);
                             break;
                         default:
                             return;
@@ -116,29 +117,32 @@ public class AlarmCreator extends AppCompatActivity {
                 hideCalendar();
             }
         });
+
+        saveButton.setOnClickListener(view -> {
+        });
     }
 
-
-    private void setRepeatDays(CompoundButton compoundButton, String day){
+    private void setRepeatDays(CompoundButton compoundButton, int day){
         if(compoundButton.isChecked()){
             repeatDays.put(day,true);
         }
         else{
             repeatDays.put(day,false);
         }
-        getRepeatDays();
+        tomorrowRingText.setText(getNextOccurrence());
     }
 
-    private void getRepeatDays(){
-        for(String day : repeatDays.keySet()){
+    private ArrayList<Integer> getRepeatDays(){
+        ArrayList<Integer> onDays = new ArrayList<>();
+        for(int day : repeatDays.keySet()){
             if(repeatDays.get(day)!=null){
                 if(repeatDays.get(day)){
-                    System.out.println(day+":"+repeatDays.get(day));
+                    onDays.add(day);
                 }
             }
         }
+        return onDays;
     }
-
 
     private void showOccurrence() {
         repeatDays.clear();
@@ -149,6 +153,32 @@ public class AlarmCreator extends AppCompatActivity {
         calendarUpButton.setVisibility(View.GONE);
         calendarDownButton.setVisibility(View.VISIBLE);
         tomorrowRingText.setVisibility(View.INVISIBLE);
+    }
+
+    private String getNextOccurrence(){
+        Calendar today = Calendar.getInstance();
+        Date date = new Date();
+        today.setTime(date);
+        int dayNumber = today.get(Calendar.DAY_OF_WEEK);
+        ArrayList<Integer> receivedOnDays = getRepeatDays();
+        Collections.sort(receivedOnDays);
+        if(receivedOnDays.size()>0){
+            if(dayNumber>Collections.max(receivedOnDays)){
+                return "This alarm will ring next "+getDay(Collections.min(receivedOnDays));
+            }
+            else if(dayNumber<Collections.min(receivedOnDays)){
+                return "This alarm will ring next "+getDay(Collections.max(receivedOnDays));
+            }
+            else if(receivedOnDays.contains(dayNumber)){
+                return "This alarm will ring today at "+getDay(dayNumber);
+            }
+            else{
+                return "This alarm will ring next "+getDay(Collections.max(receivedOnDays));
+            }
+        }
+        else{
+            return "This alarm will ring tomorrow";
+        }
     }
 
     private void resetRepeatDays(){
@@ -188,4 +218,24 @@ public class AlarmCreator extends AppCompatActivity {
         calendarDownButton.setVisibility(View.GONE);
         calendarUpButton.setVisibility(View.VISIBLE);
     }
+
+    public String getDay(int i){
+        if(i==1)
+            return "Sunday";
+        else if(i==2)
+            return "Monday";
+        else if(i==3)
+            return "Tuesday";
+        else if(i==4)
+            return "Wednesday";
+        else if(i==5)
+            return "Thursday";
+        else if(i==6)
+            return "Friday";
+        else if(i==7)
+            return "Saturday";
+        else
+            return "";
+    }
+
 }
