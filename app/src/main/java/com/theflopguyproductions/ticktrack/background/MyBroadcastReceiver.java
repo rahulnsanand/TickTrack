8ecdb79da2af35748682e6a85b745fa272be41b3
TheFlopGuy <theflopguymail@gmail.com>
2020-07-20 03:00:39 +0530

    Alarm has been setup for reboot handling!
    ToDo, launch activity on alarm trigger instead of a simple Toast.

--- app/src/main/java/com/theflopguyproductions/ticktrack/background/MyBroadcastReceiver.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/background/MyBroadcastReceiver.java
@@ -1,14 +1,17 @@
 package com.theflopguyproductions.ticktrack.background;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.theflopguyproductions.ticktrack.utils.TickTrackAlarmManager;
+
+import java.util.Objects;
+
 public class MyBroadcastReceiver extends BroadcastReceiver {
-    public static final String ACTION_ALARM = "en.proft.alarms.ACTION_ALARM";
+
+    public static final String ACTION_ALARM = "me.proft.alarms.ACTION_ALARM";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -17,6 +20,10 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
             Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
             Intent service1 = new Intent(context, MyAlarmService.class);
             context.startService(service1);
+        }
+        else if (Objects.equals(intent.getAction(), "android.intent.action.BOOT_COMPLETED")) {
+            // Set the alarm here.
+            TickTrackAlarmManager.resetAlarmOnBoot(context);
         }
 
     }
