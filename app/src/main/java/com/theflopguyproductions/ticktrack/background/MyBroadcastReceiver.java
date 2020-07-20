4eed11b856e3a5c1be867a535de15b96a5ace353
TheFlopGuy <theflopguymail@gmail.com>
2020-07-20 17:30:59 +0530

    First alarm not ringing bug fixed.
    ToDo, launch activity on alarm trigger instead of a simple Toast.

--- app/src/main/java/com/theflopguyproductions/ticktrack/background/MyBroadcastReceiver.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/background/MyBroadcastReceiver.java
@@ -3,8 +3,11 @@ package com.theflopguyproductions.ticktrack.background;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.theflopguyproductions.ticktrack.ui.home.activity.alarm.DismissAlarmActivity;
 import com.theflopguyproductions.ticktrack.utils.TickTrackAlarmManager;
 
 import java.util.Objects;
@@ -16,15 +19,29 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        System.out.println("RECEIVER RECEIVED SHIT");
+
+        Log.e("TETE", "BroadcastReceiver has received alarm intent.");
+        Intent service1 = new Intent(context, AlarmService.class);
+        context.startService(service1);
+
         if (ACTION_ALARM.equals(intent.getAction())) {
             Toast.makeText(context, "Alarm....", Toast.LENGTH_LONG).show();
-            Intent service1 = new Intent(context, MyAlarmService.class);
-            context.startService(service1);
+            getFilter();
+            Intent dismissIntent = new Intent(context, DismissAlarmActivity.class);
+            dismissIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(dismissIntent);
         }
         else if (Objects.equals(intent.getAction(), "android.intent.action.BOOT_COMPLETED")) {
             // Set the alarm here.
             TickTrackAlarmManager.resetAlarmOnBoot(context);
         }
 
+    }
+
+    public IntentFilter getFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        return filter;
     }
 }
--- app/src/main/java/com/theflopguyproductions/ticktrack/ui/home/activity/alarm/DismissAlarmActivity.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/home/activity/alarm/DismissAlarmActivity.java
@@ -2,9 +2,14 @@ package com.theflopguyproductions.ticktrack.ui.home.activity.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +25,21 @@ public class DismissAlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1)
+        {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+            KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+            if(keyguardManager!=null)
+                keyguardManager.requestDismissKeyguard(this, null);
+        }
+        else
+        {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        }
         setContentView(R.layout.activity_dismiss_alarm);
 
         dismissAlarmButton = findViewById(R.id.dismissAlarmButton);
