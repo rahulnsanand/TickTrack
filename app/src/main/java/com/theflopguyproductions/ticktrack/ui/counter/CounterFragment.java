9a941a830899ada7e31d132778477ebd963a2722
TheFlopGuy <theflopguymail@gmail.com>
2020-07-26 18:31:23 +0530

    Database Setup, Counter Fragment in progress

--- app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterFragment.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterFragment.java
@@ -11,16 +11,55 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.theflopguyproductions.ticktrack.R;
+import com.theflopguyproductions.ticktrack.counter.CounterAdapter;
+import com.theflopguyproductions.ticktrack.counter.CounterData;
+import com.theflopguyproductions.ticktrack.utils.TickTrackDatabase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
 
 public class CounterFragment extends Fragment {
+
+    private ArrayList<CounterData> counterDataArrayList = new ArrayList<>();
+    private RecyclerView counterRecyclerView;
+    private CounterAdapter counterAdapter;
+    private FloatingActionButton counterFab;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_counter, container, false);
 
+        counterDataArrayList = TickTrackDatabase.retrieveCounterList(requireActivity());
+        counterRecyclerView = root.findViewById(R.id.counterRecycleView);
+        buildRecyclerView();
+        counterFab = root.findViewById(R.id.counterAddButton);
+
+        counterFab.setOnClickListener(view -> {
+            
+        });
+
         return root;
     }
+
+    private void buildRecyclerView() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        counterRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        counterRecyclerView.setHasFixedSize(true);
+
+        Collections.sort(counterDataArrayList);
+
+        counterAdapter = new CounterAdapter(counterDataArrayList);
+        counterRecyclerView.setLayoutManager(layoutManager);
+        counterRecyclerView.setAdapter(counterAdapter);
+        counterAdapter.notifyDataSetChanged();
+    }
+
 }
---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/utils/TickTrackDatabase.java
@@ -0,0 +1,70 @@
+package com.theflopguyproductions.ticktrack.utils;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.theflopguyproductions.ticktrack.counter.CounterData;
+import com.theflopguyproductions.ticktrack.timer.TimerData;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class TickTrackDatabase {
+
+    public static void storeCounterList(ArrayList<CounterData> counterDataArrayList, Activity activity){
+
+        SharedPreferences sharedPreferences = activity.getSharedPreferences("TickTrackData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(counterDataArrayList);
+        editor.putString("CounterData", json);
+        editor.apply();
+
+    }
+
+    public static ArrayList<CounterData> retrieveCounterList(Activity activity){
+
+        SharedPreferences sharedPreferences = activity.getSharedPreferences("TickTrackData", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("CounterData", null);
+        Type type = new TypeToken<ArrayList<CounterData>>() {}.getType();
+        ArrayList<CounterData> counterDataArrayList = gson.fromJson(json, type);
+
+        if(counterDataArrayList == null){
+            counterDataArrayList = new ArrayList<>();
+        }
+
+        return counterDataArrayList;
+    }
+
+    public static void storeTimerList(ArrayList<TimerData> counterDataArrayList, Activity activity){
+
+        SharedPreferences sharedPreferences = activity.getSharedPreferences("TickTrackData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(counterDataArrayList);
+        editor.putString("TimerData", json);
+        editor.apply();
+
+    }
+
+    public static ArrayList<TimerData> retrieveTimerList(Activity activity){
+
+        SharedPreferences sharedPreferences = activity.getSharedPreferences("TickTrackData", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("TimerData", null);
+        Type type = new TypeToken<ArrayList<TimerData>>() {}.getType();
+        ArrayList<TimerData> counterDataArrayList = gson.fromJson(json, type);
+
+        if(counterDataArrayList == null){
+            counterDataArrayList = new ArrayList<>();
+        }
+
+        return counterDataArrayList;
+    }
+
+}
