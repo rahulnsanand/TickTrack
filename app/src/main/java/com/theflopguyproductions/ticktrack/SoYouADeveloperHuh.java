351c79bc3193124f64a49e3eb9b46192e2476512
TheFlopGuy <theflopguymail@gmail.com>
2020-07-26 17:57:40 +0530

    Menu Item setup, Counter Data setup

--- app/src/main/java/com/theflopguyproductions/ticktrack/SoYouADeveloperHuh.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/SoYouADeveloperHuh.java
@@ -3,13 +3,24 @@ package com.theflopguyproductions.ticktrack;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.theflopguyproductions.ticktrack.ui.counter.CounterFragment;
+import com.theflopguyproductions.ticktrack.ui.stopwatch.StopwatchFragment;
+import com.theflopguyproductions.ticktrack.ui.timer.TimerFragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,15 +28,78 @@ import androidx.navigation.ui.NavigationUI;
 
 public class SoYouADeveloperHuh extends AppCompatActivity {
 
+    private Toolbar mainToolbar;
+    private BottomNavigationView navView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_so_you_a_developer_huh);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+
+        navView = findViewById(R.id.nav_view);
+        mainToolbar = findViewById(R.id.mainActivityToolbar);
+
         navView.setItemIconTintList(null);
+        navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        overflowMenuSetup();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_counter:
+                openFragment(new CounterFragment());
+                return true;
+
+            case R.id.navigation_stopwatch:
+                openFragment(new StopwatchFragment());
+                return true;
+
+            case R.id.navigation_timer:
+                openFragment(new TimerFragment());
+                return true;
+        }
+        return false;
+    };
 
 
 
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void overflowMenuSetup(){
+        mainToolbar.inflateMenu(R.menu.overflow_menu);
+
+        mainToolbar.setOnMenuItemClickListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.screensaverMenuItem:
+                    Toast.makeText(getApplicationContext(),"Screensaver",Toast.LENGTH_SHORT).show();
+                    return false;
+
+                case R.id.settingsMenuItem:
+                    Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                    return false;
+
+                case R.id.feedbackMenuItem:
+                    Toast.makeText(getApplicationContext(),"Feedback",Toast.LENGTH_SHORT).show();
+                    return false;
+
+                case R.id.aboutMenuItem:
+                    Toast.makeText(getApplicationContext(),"About",Toast.LENGTH_SHORT).show();
+                    return true;
+
+                default:
+                    break;
+            }
+            return false;
+        });
     }
 
 }
--- app/src/main/java/com/theflopguyproductions/ticktrack/counter/CounterData.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/counter/CounterData.java
@@ -1,7 +1,47 @@
 package com.theflopguyproductions.ticktrack.counter;
 
-public class CounterData {
+import java.sql.Timestamp;
 
+public class CounterData implements Comparable<CounterData>{
 
+    int counterValue, counterFlag;
+    String counterLabel;
+    Timestamp counterTimestamp;
 
+    public int getCounterValue() {
+        return counterValue;
+    }
+
+    public void setCounterValue(int counterValue) {
+        this.counterValue = counterValue;
+    }
+
+    public int getCounterFlag() {
+        return counterFlag;
+    }
+
+    public void setCounterFlag(int counterFlag) {
+        this.counterFlag = counterFlag;
+    }
+
+    public String getCounterLabel() {
+        return counterLabel;
+    }
+
+    public void setCounterLabel(String counterLabel) {
+        this.counterLabel = counterLabel;
+    }
+
+    public Timestamp getCounterTimestamp() {
+        return counterTimestamp;
+    }
+
+    public void setCounterTimestamp(Timestamp counterTimestamp) {
+        this.counterTimestamp = counterTimestamp;
+    }
+
+    @Override
+    public int compareTo(CounterData counterData) {
+        return 0;
+    }
 }
