8ea11f9cab940625cf49716fb178f12897a56a4f
TheFlopGuy <theflopguymail@gmail.com>
2020-08-13 02:09:42 +0530

    Stop watch design patterns completed Research stopwatch working started
    Yet to implement,
    Timer vibrate pattern
    Timer ring time
    Timer start time
    Entire StopWatch
    
    Yet to fix,
    TimerNotification Persistence, Vibration, Sound uplift, headsup quality on MIUI

---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/stopwatch/StopwatchData.java
@@ -0,0 +1,8 @@
+package com.theflopguyproductions.ticktrack.stopwatch;
+
+public class StopwatchData {
+
+    int stopwatchID;
+    boolean isRunning, isPause, isNotification, lapsExist;
+
+}
--- app/src/main/java/com/theflopguyproductions/ticktrack/ui/stopwatch/StopwatchFragment.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/stopwatch/StopwatchFragment.java
@@ -1,5 +1,6 @@
 package com.theflopguyproductions.ticktrack.ui.stopwatch;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,21 +9,74 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.theflopguyproductions.ticktrack.R;
+import com.theflopguyproductions.ticktrack.ui.utils.TickTrackProgressBar;
+import com.theflopguyproductions.ticktrack.utils.TickTrackDatabase;
+import com.theflopguyproductions.ticktrack.utils.TickTrackThemeSetter;
 
 public class StopwatchFragment extends Fragment {
+
+    private ConstraintLayout stopwatchRootLayout, stopwatchLapLayout;
+    private TextView stopwatchValueText, stopwatchLapTitleText;
+    private RecyclerView stopwatchLapRecyclerView;
+    private TickTrackProgressBar foregroundProgressBar, backgroundProgressBar;
+    private FloatingActionButton playPauseFAB, flagFAB, resetFAB;
+
+    private Activity activity;
+    private TickTrackDatabase tickTrackDatabase;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_stopwatch, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+        activity = getActivity();
+        initVariables(root);
+        initValues();
+
+        setupClickListeners();
 
         return root;
+    }
+
+    private void setupClickListeners() {
+
+
+
+    }
+
+    private void initValues() {
+        tickTrackDatabase = new TickTrackDatabase(activity);
+    }
+
+    private void initVariables(View parent) {
+
+        stopwatchRootLayout = parent.findViewById(R.id.stopwatchRootLayout);
+        stopwatchLapLayout = parent.findViewById(R.id.stopwatchFragmentLapLayout);
+        stopwatchValueText = parent.findViewById(R.id.stopwatchFragmentValueTextView);
+        stopwatchLapTitleText = parent.findViewById(R.id.stopwatchFragmentLapTextView);
+        stopwatchLapRecyclerView = parent.findViewById(R.id.stopwatchFragmentRecyclerView);
+        foregroundProgressBar = parent.findViewById(R.id.stopwatchFragmentProgressForeground);
+        backgroundProgressBar = parent.findViewById(R.id.stopwatchFragmentProgressBackground);
+        playPauseFAB = parent.findViewById(R.id.stopwatchFragmentPlayPauseFAB);
+        flagFAB = parent.findViewById(R.id.stopwatchFragmentFlagFAB);
+        resetFAB = parent.findViewById(R.id.stopwatchFragmentResetFAB);
+
+        backgroundProgressBar.setInstantProgress(1);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TickTrackThemeSetter.stopwatchFragmentTheme(activity, stopwatchRootLayout, stopwatchLapTitleText, stopwatchValueText,
+                tickTrackDatabase, backgroundProgressBar);
     }
 }
