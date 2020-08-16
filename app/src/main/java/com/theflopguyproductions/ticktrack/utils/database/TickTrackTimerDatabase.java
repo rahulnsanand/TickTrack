aaa827faaec1f320b4ab291a56600bf82a47cf21
TheFlopGuy <theflopguymail@gmail.com>
2020-08-17 03:30:31 +0530

    Fixed timer working for below 5 seconds
    Fixed animation, added Roboto for Number, May implement for entire app other than appName
    Timer recyclerview to activity lag in duration left.
    Counter Edit save button takes to wrong counter.
    Overflow menu button decide icon
    
    Yet to implement,
    Timer vibrate pattern
    Timer ring time
    Timer start time
    Boot operations
    
    Yet to fix,
    TimerNotification Persistence, Vibration, Sound uplift, headsup quality on MIUI

--- app/src/main/java/com/theflopguyproductions/ticktrack/utils/database/TickTrackTimerDatabase.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/utils/database/TickTrackTimerDatabase.java
@@ -5,6 +5,8 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
 
 import com.theflopguyproductions.ticktrack.timer.service.TimerBroadcastReceiver;
 import com.theflopguyproductions.ticktrack.timer.service.TimerService;
@@ -19,17 +21,28 @@ public class TickTrackTimerDatabase {
 
     public void setAlarm(long endTime, int timerIntegerID){
 
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, TimerBroadcastReceiver.class);
-        intent.setAction(TimerBroadcastReceiver.ACTION_TIMER_BROADCAST);
-        intent.putExtra("timerIntegerID", timerIntegerID);
-        PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(context, timerIntegerID, intent, 0);
-        alarmManager.setExact(
-                AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                endTime,
-                alarmPendingIntent
-        );
-        System.out.println("TimerCreateAlarm");
+        long shortTime = endTime - SystemClock.elapsedRealtime();
+        System.out.println(shortTime+ "<<<<<<<<<<<<<<<<<<");
+        if(!(shortTime > 5000)){
+            new Handler().postDelayed(() -> {
+                Intent intent = new Intent(context, TimerBroadcastReceiver.class);
+                intent.setAction(TimerBroadcastReceiver.ACTION_TIMER_BROADCAST);
+                intent.putExtra("timerIntegerID", timerIntegerID);
+                this.context.sendBroadcast(intent);
+            }, shortTime);
+
+        } else {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(context, TimerBroadcastReceiver.class);
+            intent.setAction(TimerBroadcastReceiver.ACTION_TIMER_BROADCAST);
+            intent.putExtra("timerIntegerID", timerIntegerID);
+            PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(context, timerIntegerID, intent, 0);
+            alarmManager.setExact(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    endTime,
+                    alarmPendingIntent
+            );
+        }
     }
 
     public void cancelAlarm(int timerIntegerID){
@@ -38,7 +51,6 @@ public class TickTrackTimerDatabase {
         intent.setAction(TimerBroadcastReceiver.ACTION_TIMER_BROADCAST);
         PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(context, timerIntegerID, intent, 0);
         alarmManager.cancel(alarmPendingIntent);
-        System.out.println("TimerCancelledAlarm");
     }
 
     public void startNotificationService() {
