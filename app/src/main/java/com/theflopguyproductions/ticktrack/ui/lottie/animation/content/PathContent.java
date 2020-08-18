e7e7455b270c6ed2de83176bc4d85449784515dc
TheFlopGuy <theflopguymail@gmail.com>
2020-08-19 02:15:05 +0530

    Basic implementationd done. Shit Lottie Anim yet to fix.
    lottie fixed.
    
    Timer recyclerview to activity lag in duration left.
    Overflow menu button decide icon
    
    Yet to implement,
    Timer vibrate pattern
    Timer ring time
    Timer start time
    Boot operations
    
    Yet to fix,
    TimerNotification Persistence, Vibration, Sound uplift, headsup quality on MIUI

---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/lottie/animation/content/PathContent.java
@@ -0,0 +1,7 @@
+package com.theflopguyproductions.ticktrack.ui.lottie.animation.content;
+
+import android.graphics.Path;
+
+interface PathContent extends Content {
+  Path getPath();
+}
--- app/src/main/java/com/theflopguyproductions/ticktrack/utils/lottie/animation/content/PathContent.java
+++
@@ -1,9 +0,0 @@
-package com.theflopguyproductions.ticktrack.ui.lottie.animation.content;
-
-import android.graphics.Path;
-
-import com.theflopguyproductions.ticktrack.ui.lottie.animation.content.Content;
-
-interface PathContent extends Content {
-  Path getPath();
-}
