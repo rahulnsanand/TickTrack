2793ec9ff46ad95b313f6639af01eca8b02a0321
TheFlopGuy <theflopguymail@gmail.com>
2020-08-19 00:51:42 +0530

    Init startup activity creation
    Timer recyclerview to activity lag in duration left.
    Overflow menu button decide icon
    
    Yet to implement,
    Timer vibrate pattern
    Timer ring time
    Timer start time
    Boot operations
    
    Yet to fix,
    TimerNotification Persistence, Vibration, Sound uplift, headsup quality on MIUI

---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/startup/fragments/AutoStartFragment.java
@@ -0,0 +1,6 @@
+package com.theflopguyproductions.ticktrack.startup.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class AutoStartFragment extends Fragment {
+}
---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/startup/fragments/IntroFragment.java
@@ -0,0 +1,50 @@
+package com.theflopguyproductions.ticktrack.startup.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.theflopguyproductions.ticktrack.R;
+
+public class IntroFragment extends Fragment {
+
+    private OnGetStartedClickListener getStartedClickListener;
+
+    private Button getStartedButton;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_ticktrack_intro, container, false);
+        getStartedButton = root.findViewById(R.id.TickTrackIntroGetStartedButton);
+
+        getStartedButton.setOnClickListener(view -> getStartedClickListener.onGetStartedClick());
+
+        return root;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            getStartedClickListener = (OnGetStartedClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement " + OnGetStartedClickListener.class.getName());
+        }
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        getStartedClickListener = null;
+    }
+    public interface OnGetStartedClickListener {
+        void onGetStartedClick();
+    }
+}
