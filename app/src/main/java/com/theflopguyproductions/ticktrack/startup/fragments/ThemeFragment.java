de28b86c849896c0bfa1ea8f1d3237993bd8e0a4
TheFlopGuy <theflopguymail@gmail.com>
2020-08-20 20:55:25 +0530

    Fixing theme buttons
    lottie fixed.
    
    Timer recyclerview to activity lag in duration left.
    Overflow menu button decide icon
    
    Yet to implement,
    Timer vibrate pattern
    Timer ring time
    Timer start time
    Boot operations
    
    Yet to fix,
    TimerNotification Persistence, Vibration, Sound uplift, headsup quality on MIUI

--- app/src/main/java/com/theflopguyproductions/ticktrack/startup/fragments/ThemeFragment.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/startup/fragments/ThemeFragment.java
@@ -6,11 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.theflopguyproductions.ticktrack.R;
+import com.theflopguyproductions.ticktrack.utils.database.TickTrackDatabase;
 
 public class ThemeFragment extends Fragment {
 
@@ -18,15 +21,47 @@ public class ThemeFragment extends Fragment {
 
     private Button themeSetButton;
 
+    private ImageButton darkThemeButton, lightThemeButton;
+    private TickTrackDatabase tickTrackDatabase;
+    private int themeMode = 1;
+    private ConstraintLayout rootLayout;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_ticktrack_theme, container, false);
-        themeSetButton = root.findViewById(R.id.ticktrackThemeButton);
+        themeSetButton = root.findViewById(R.id.ticktrackFragmentThemeContinueButton);
+        darkThemeButton = root.findViewById(R.id.ticktrackFragmentThemeDarkThemeButton);
+        lightThemeButton = root.findViewById(R.id.ticktrackFragmentThemeLightThemeButton);
+
+        rootLayout = root.findViewById(R.id.ticktrackFragmentThemeRoot);
+        tickTrackDatabase = new TickTrackDatabase(requireContext());
+        setupTheme();
 
         themeSetButton.setOnClickListener(view -> onThemeSetClickListener.onThemeSetClickListener());
-
         return root;
+    }
+
+//    private void reverseAnimate() {
+//        float progress = lottieAnimationView.getProgress();
+//        ValueAnimator valueAnimator = ValueAnimator.ofFloat(-progress,0 ).setDuration((long) ( lottieAnimationView.getDuration()* progress));
+//        valueAnimator.addUpdateListener(animation -> lottieAnimationView.setProgress(Math.abs((float)animation.getAnimatedValue())));
+//        valueAnimator.start();
+//    }
+
+    private void setupTheme() {
+        themeMode = tickTrackDatabase.getThemeMode();
+        if(themeMode==1){
+            lightThemeButton.setClickable(false);
+            darkThemeButton.setClickable(true);
+            rootLayout.setBackgroundResource(R.color.LightGray);
+        } else {
+            darkThemeButton.setClickable(false);
+            lightThemeButton.setClickable(true);
+
+            rootLayout.setBackgroundResource(R.color.Black);
+        }
+
     }
 
     public interface OnThemeSetClickListener {
