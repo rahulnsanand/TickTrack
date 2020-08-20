b9199883763416c99ad714be647b5a26455e5ae7
TheFlopGuy <theflopguymail@gmail.com>
2020-08-20 14:41:45 +0530

    Perfected Logo Animation
    lottie fixed.
    
    Timer recyclerview to activity lag in duration left.
    Overflow menu button decide icon
    
    Yet to implement,
    Timer vibrate pattern
    Timer ring time
    Timer start time
    Boot operations
    
    Yet to fix,
    TimerNotification Persistence, Vibration, Sound uplift, headsup quality on MIUI

--- app/src/main/java/com/theflopguyproductions/ticktrack/startup/fragments/IntroFragment.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/startup/fragments/IntroFragment.java
@@ -1,5 +1,6 @@
 package com.theflopguyproductions.ticktrack.startup.fragments;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,19 +12,46 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.theflopguyproductions.ticktrack.R;
+import com.theflopguyproductions.ticktrack.ui.lottie.LottieAnimationView;
 
 public class IntroFragment extends Fragment {
 
     private OnGetStartedClickListener getStartedClickListener;
 
     private Button getStartedButton;
+    private LottieAnimationView lottieAnimationView;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_ticktrack_intro, container, false);
         getStartedButton = root.findViewById(R.id.TickTrackIntroGetStartedButton);
+        getStartedButton.setVisibility(View.INVISIBLE);
+        lottieAnimationView = root.findViewById(R.id.tickTrackIntroLogoAnimation);
+        lottieAnimationView.setAnimation(R.raw.logo_animation_ticktrack);
+        lottieAnimationView.playAnimation();
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
 
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                getStartedButton.setVisibility(View.VISIBLE);
+                lottieAnimationView.playAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
         getStartedButton.setOnClickListener(view -> getStartedClickListener.onGetStartedClick());
 
         return root;
