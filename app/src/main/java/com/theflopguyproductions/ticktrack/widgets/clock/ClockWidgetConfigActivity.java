e9aa05914737ecb349cb38d38681fac903f368c1
TheFlopGuy <theflopguymail@gmail.com>
2020-09-10 02:09:05 +0530

    Clock Config Layout Design Complete

--- app/src/main/java/com/theflopguyproductions/ticktrack/widgets/clock/ClockWidgetConfigActivity.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/widgets/clock/ClockWidgetConfigActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.theflopguyproductions.ticktrack.R;
+
 public class ClockWidgetConfigActivity extends AppCompatActivity {
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
