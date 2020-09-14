e656eb688ca255a8a84826c67cf88b34e0319573
TheFlopGuy <theflopguymail@gmail.com>
2020-09-14 17:08:19 +0530

    Added settings button screensaver setup complete.

--- app/src/main/java/com/theflopguyproductions/ticktrack/screensaver/ScreensaverActivity.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/screensaver/ScreensaverActivity.java
@@ -4,22 +4,63 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.theflopguyproductions.ticktrack.R;
 import com.theflopguyproductions.ticktrack.SoYouADeveloperHuh;
+import com.theflopguyproductions.ticktrack.settings.SettingsActivity;
 import com.theflopguyproductions.ticktrack.utils.database.TickTrackDatabase;
 
 public class ScreensaverActivity extends AppCompatActivity {
 
     private ConstraintLayout rootLayout;
-    private int dialogClock, hourHandClock, minuteHandClock;
-    LayoutInflater layoutInflater;
-    ConstraintLayout rr;
+    private LayoutInflater layoutInflater;
+    private ConstraintLayout clockLayout;
+    private ConstraintLayout buttonLayout;
+    private Handler optionsDisplayHandler;
+    private TextView dismissTextHelper;
+    private Button settingsButton;
+
+    private Runnable optionsDisplayRunnable = new Runnable() {
+        long currentTime = SystemClock.elapsedRealtime();
+        @Override
+        public void run() {
+            long loopTime = SystemClock.elapsedRealtime();
+            long difference = loopTime-currentTime;
+            if(difference<3000){
+                optionsDisplayHandler.post(optionsDisplayRunnable);
+            } else {
+                hideOptionsDisplay();
+                optionsDisplayHandler.removeCallbacks(optionsDisplayRunnable);
+            }
+        }
+    };
+
+    private boolean isOptionsOpen = false;
+    private void hideOptionsDisplay() {
+        if(isOptionsOpen){
+            dismissTextHelper.setVisibility(View.GONE);
+            buttonLayout.setVisibility(View.GONE);
+            isOptionsOpen = false;
+            optionsDisplayHandler.removeCallbacks(optionsDisplayRunnable);
+        }
+    }
+    private void showOptionsDisplay() {
+        if(!isOptionsOpen){
+            optionsDisplayHandler.post(optionsDisplayRunnable);
+            buttonLayout.setVisibility(View.VISIBLE);
+            dismissTextHelper.setVisibility(View.VISIBLE);
+            isOptionsOpen = true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +68,21 @@ public class ScreensaverActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_screensaver);
         rootLayout = findViewById(R.id.screenSaverRootLayout);
+        buttonLayout = findViewById(R.id.screensaverButtonLayout);
+        dismissTextHelper = findViewById(R.id.screensaverDismissText);
+        settingsButton = findViewById(R.id.screensaverEditButton);
 
-        rootLayout.setOnClickListener(view ->
-                startActivity(new Intent(this, SoYouADeveloperHuh.class)));
+        rootLayout.setOnClickListener(view ->{
+            if(isOptionsOpen){
+                startActivity(new Intent(this, SoYouADeveloperHuh.class));
+            } else {
+                showOptionsDisplay();
+            }
+        });
+
+        settingsButton.setOnClickListener(view -> {
+                startActivityForResult(new Intent(this, SettingsActivity.class), 1012);
+        });
 
         getWindow().setStatusBarColor(getResources().getColor(R.color.Accent));
 
@@ -49,13 +102,15 @@ public class ScreensaverActivity extends AppCompatActivity {
                 Context.LAYOUT_INFLATER_SERVICE);
         layoutInflater = LayoutInflater.from(ScreensaverActivity.this);
 
-        rr = (ConstraintLayout) findViewById(R.id.clockContainer);
+        clockLayout = (ConstraintLayout) findViewById(R.id.clockContainer);
         setupClock(style);
 
     }
 
+
+
     private void setupClock(int style) {
-        rr.removeAllViews();
+        clockLayout.removeAllViews();
         final  View v;
         if(style==1){
             v = layoutInflater.inflate(R.layout.tick_track_clock_widget1, null);
@@ -72,9 +127,14 @@ public class ScreensaverActivity extends AppCompatActivity {
         } else {
             v = layoutInflater.inflate(R.layout.tick_track_clock_widget1, null);
         }
-        rr.addView(v);
+        clockLayout.addView(v);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        optionsDisplayHandler.removeCallbacks(optionsDisplayRunnable);
+    }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
