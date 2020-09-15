1b7a8ca3a1372c776c559e0f9f37c81097d2b3f1
TheFlopGuy <theflopguymail@gmail.com>
2020-09-15 23:03:53 +0530

    Counter Sum Code Completed. Bugs yet to fix
    Minor Font Change Left.

--- app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterFragment.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/ui/counter/CounterFragment.java
@@ -33,6 +33,9 @@ import com.theflopguyproductions.ticktrack.utils.helpers.TickTrackThemeSetter;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class CounterFragment extends Fragment implements CounterSlideDeleteHelper.RecyclerItemTouchHelperListener{
 
@@ -68,8 +71,13 @@ public class CounterFragment extends Fragment implements CounterSlideDeleteHelpe
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onResume() {
+        super.onResume();
+        sharedPreferences = tickTrackDatabase.getSharedPref(activity);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
+        if("counterCreate".equals(receivedAction)){
+            createCounterDialog(activity);
+        }
         TickTrackAnimator.fabLayoutUnDissolve(counterFab);
         if(tickTrackDatabase.isSumEnabled()){
             setupSumLayout();
@@ -78,18 +86,7 @@ public class CounterFragment extends Fragment implements CounterSlideDeleteHelpe
             sumValue.setVisibility(View.GONE);
         }
         TickTrackThemeSetter.counterFragmentTheme(getActivity(), counterRecyclerView, counterFragmentRootLayout, noCounterText, tickTrackDatabase, sumLayout);
-        System.out.println("ActivityManager: Displayed CounterFrag OnStart "+System.currentTimeMillis());
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        sharedPreferences = tickTrackDatabase.getSharedPref(activity);
-        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
-        if("counterCreate".equals(receivedAction)){
-            createCounterDialog(activity);
-        }
-        System.out.println("ActivityManager: Displayed CounterFrag OnResume "+System.currentTimeMillis());
+        sumLayout.setOnClickListener(view -> sumValue.setText("Sum: "+sum));
     }
 
     private static void createCounterDialog(Activity context){
@@ -99,11 +96,21 @@ public class CounterFragment extends Fragment implements CounterSlideDeleteHelpe
         });
     }
 
+    private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+    static {
+        suffixes.put(1_000L, "k");
+        suffixes.put(1_000_000L, "M");
+        suffixes.put(1_000_000_000L, "G");
+        suffixes.put(1_000_000_000_000L, "T");
+        suffixes.put(1_000_000_000_000_000L, "P");
+        suffixes.put(1_000_000_000_000_000_000L, "E");
+    }
+    private long sum = 0;
     private void setupSumLayout() {
         TickTrackAnimator.layoutUnDissolve(sumLayout);
         sumValue.setVisibility(View.VISIBLE);
 
-        long sum = 0L;
+        sum = 0L;
         counterDataArrayList = tickTrackDatabase.retrieveCounterList();
         for(int i=0; i<counterDataArrayList.size(); i++){
             if(counterDataArrayList.get(i).getCounterValue()>0){
@@ -112,8 +119,24 @@ public class CounterFragment extends Fragment implements CounterSlideDeleteHelpe
                 }
             }
         }
-        sumValue.setText("Sum: "+sum);
+        sumValue.setText("Sum: "+format(sum));
     }
+
+    public static String format(long value) {
+        //Long.MIN_VALUE == -Long.MIN_VALUE so we need an adjustment here
+        if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
+        if (value < 0) return "-" + format(-value);
+        if (value < 1000) return Long.toString(value); //deal with easy case
+
+        Map.Entry<Long, String> e = suffixes.floorEntry(value);
+        Long divideBy = e.getKey();
+        String suffix = e.getValue();
+
+        long truncated = value / (divideBy / 10); //the number part of the output times 10
+        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
+    }
+
 
     SharedPreferences.OnSharedPreferenceChangeListener sharedPreferenceChangeListener = (sharedPreferences, s) ->  {
         counterDataArrayList = tickTrackDatabase.retrieveCounterList();
