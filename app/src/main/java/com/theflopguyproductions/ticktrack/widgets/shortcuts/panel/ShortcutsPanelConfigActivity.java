d1383539dc23aa475b225f8bd581e0ae6e6fed15
TheFlopGuy <theflopguymail@gmail.com>
2020-09-17 01:38:19 +0530

    Designing Shortcut Panel Complete

---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/widgets/shortcuts/panel/ShortcutsPanelConfigActivity.java
@@ -0,0 +1,16 @@
+package com.theflopguyproductions.ticktrack.widgets.shortcuts.panel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.theflopguyproductions.ticktrack.R;
+
+public class ShortcutsPanelConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shortcuts_panel_config);
+    }
+}
