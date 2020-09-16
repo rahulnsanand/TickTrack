b48735f015d5e22a1a8f73c80511d34d6353dc2e
TheFlopGuy <theflopguymail@gmail.com>
2020-09-17 03:19:50 +0530

    Perfected all shortcuts

--- app/src/main/java/com/theflopguyproductions/ticktrack/widgets/shortcuts/StopwatchWidget.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/widgets/shortcuts/StopwatchWidget.java
@@ -11,32 +11,32 @@ import com.theflopguyproductions.ticktrack.R;
 import com.theflopguyproductions.ticktrack.SoYouADeveloperHuh;
 import com.theflopguyproductions.ticktrack.utils.database.TickTrackDatabase;
 
-
-public class CreateTimerWidget extends AppWidgetProvider {
+public class StopwatchWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.create_timer_widget);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stopwatch_widget);
         TickTrackDatabase tickTrackDatabase = new TickTrackDatabase(context);
 
         int theme = tickTrackDatabase.getThemeMode();
 
         if(theme==1){
-            views.setInt(R.id.createTimerShortcutWidgetRootLayout, "setBackgroundResource", R.drawable.round_rect_light);
+            views.setInt(R.id.createStopwatchShortcutWidgetRootLayout, "setBackgroundResource", R.drawable.round_rect_light);
         } else {
-            views.setInt(R.id.createTimerShortcutWidgetRootLayout, "setBackgroundResource", R.drawable.round_rect_dark);
+            views.setInt(R.id.createStopwatchShortcutWidgetRootLayout, "setBackgroundResource", R.drawable.round_rect_dark);
         }
 
         Intent createTimerIntent = new Intent(context, SoYouADeveloperHuh.class);
-        createTimerIntent.putExtra("fragmentID","timerCreate");
+        createTimerIntent.putExtra("fragmentID","stopwatchCreate");
         createTimerIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        views.setOnClickPendingIntent(R.id.createTimerShortcutWidgetRootLayout, PendingIntent.getActivity(context, 74, createTimerIntent, 0));
+        views.setOnClickPendingIntent(R.id.createStopwatchShortcutWidgetRootLayout, PendingIntent.getActivity(context, 77, createTimerIntent, 0));
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
@@ -44,10 +44,12 @@ public class CreateTimerWidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
     }
 
     @Override
     public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
     }
 }
 
