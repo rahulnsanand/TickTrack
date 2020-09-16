40c18c022f18ae759d75ab29c1b785e6d49cc69f
TheFlopGuy <theflopguymail@gmail.com>
2020-09-17 02:02:50 +0530

    Shortcut panel design Complete

--- app/src/main/java/com/theflopguyproductions/ticktrack/widgets/shortcuts/panel/ShortcutsPanelConfigActivity.java
+++ app/src/main/java/com/theflopguyproductions/ticktrack/widgets/shortcuts/panel/ShortcutsPanelConfigActivity.java
@@ -1,12 +1,18 @@
 package com.theflopguyproductions.ticktrack.widgets.shortcuts.panel;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.theflopguyproductions.ticktrack.R;
 
 public class ShortcutsPanelConfigActivity extends AppCompatActivity {
+
+    private TextView blackText, grayText, lightText;
+    private ImageView blackImage, grayImage, lightImage;
+    private ImageView blackCheck, grayCheck, lightCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
