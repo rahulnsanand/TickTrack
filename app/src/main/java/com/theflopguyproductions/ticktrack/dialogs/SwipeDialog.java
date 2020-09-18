0c384aa97d1a0eb03ba8331c2bd35b0ab31afe63
TheFlopGuy <theflopguymail@gmail.com>
2020-09-19 04:18:12 +0530

    Designing of SwipeDialog complete. Yet to implement it in restore fragment and settings activity

---
+++ app/src/main/java/com/theflopguyproductions/ticktrack/dialogs/SwipeDialog.java
@@ -0,0 +1,81 @@
+package com.theflopguyproductions.ticktrack.dialogs;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.theflopguyproductions.ticktrack.R;
+import com.theflopguyproductions.ticktrack.ui.utils.swipebutton.SwipeButton;
+import com.theflopguyproductions.ticktrack.utils.database.TickTrackDatabase;
+
+import java.util.Objects;
+
+public class SwipeDialog extends BottomSheetDialog {
+
+    private Activity activity;
+    private TickTrackDatabase tickTrackDatabase;
+    private ConstraintLayout rootLayout;
+    public TextView dialogTitle, dialogMessage;
+    public SwipeButton swipeButton;
+    int themeSet = 1;
+
+    public SwipeDialog(Activity activity){
+        super(activity);
+        this.activity = activity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view = activity.getLayoutInflater().inflate(R.layout.dialog_swipe, new ConstraintLayout(activity), false);
+        setContentView(view);
+        Objects.requireNonNull(getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager wm = (WindowManager) activity.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = Math.min(metrics.widthPixels, 1280);
+        int height = -1; // MATCH_PARENT
+        getWindow().setLayout(width, height);
+
+        initVariables(view);
+
+        tickTrackDatabase = new TickTrackDatabase(getContext());
+        themeSet = tickTrackDatabase.getThemeMode();
+
+        setupTheme();
+    }
+
+    private void setupTheme() {
+
+        if(themeSet==1){
+            rootLayout.setBackgroundResource(R.color.LightGray);
+            dialogMessage.setTextColor(activity.getResources().getColor(R.color.LightDarkText));
+
+        } else {
+            rootLayout.setBackgroundResource(R.color.Gray);
+            dialogMessage.setTextColor(activity.getResources().getColor(R.color.DarkLightText));
+        }
+    }
+
+    private void initVariables(View view) {
+        dialogMessage = view.findViewById(R.id.swipeDialogMessageText);
+        rootLayout = view.findViewById(R.id.swipeDialogRootLayout);
+        dialogTitle = view.findViewById(R.id.swipeDialogTitleText);
+        swipeButton = view.findViewById(R.id.swipeDialogSwipeButton);
+    }
+
+
+}
